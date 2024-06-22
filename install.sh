8ee55b33265678e96425615b0021f1cecde5018e
daemy <ilioskidimitar98@gmail.com>
2024-06-22 04:04:48 +0200

    added linux binary + installation script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Copy binary to /usr/local/bin
+sudo cp -r bin/docker-manager /usr/local/bin/
+
+echo "Installation complete. You can run the application using the command 'docker-manager [parameters]'"
+
